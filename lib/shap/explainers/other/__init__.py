860c386280845f82255a3960388b02f5281752ed
Muddassar Sharif <ms8909@nyu.edu>
2020-07-17 12:11:51 +0500

    shap folder removed

--- lib/shap/explainers/other/__init__.py
+++
@@ -1,5 +0,0 @@
-from .coefficent import CoefficentExplainer
-from .random import RandomExplainer
-from .lime import LimeTabularExplainer
-from .maple import MapleExplainer, TreeMapleExplainer
-from .treegain import TreeGainExplainer
