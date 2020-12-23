83f4184160b20040cab97f1931aecd3f415ea4b7
Raheel Ahmad <rab657@nyu.edu>
2020-12-23 17:43:09 -0500

    Update app.py

--- lib/app.py
+++ lib/app.py
@@ -27,16 +27,26 @@ app.index_string = '''
         {%favicon%}
         {%css%}
         {%scripts%}
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:2146099,hjsv:6};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
     </head>
     <body>
-        <div>My Custom header</div>
+       
         {%app_entry%}
         <footer>
             {%config%}
            
             {%renderer%}
         </footer>
-        <div>My Custom footer</div>
+       
     </body>
 </html>
 '''
