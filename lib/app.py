3b2540593655639e5825813d9f2802039020acca
Raheel Ahmad <rab657@nyu.edu>
2020-12-23 16:42:56 -0500

    Update app.py

--- lib/app.py
+++ lib/app.py
@@ -18,6 +18,29 @@ external_scripts = ['https://raw.githubusercontent.com/explainX/explainx/share_b
 
 app = JupyterDash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True, external_scripts=external_scripts)
 
+app.index_string = '''
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+        {%scripts%}
+    </head>
+    <body>
+        <div>My Custom header</div>
+        {%app_entry%}
+        <footer>
+            {%config%}
+           
+            {%renderer%}
+        </footer>
+        <div>My Custom footer</div>
+    </body>
+</html>
+'''
+
 app.title = "explainX.ai - Main Dashboard"
 
 
